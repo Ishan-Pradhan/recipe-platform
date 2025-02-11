9d1975b5b5b8c0b55827cb7c25e3bf9bbcf14e60
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-02-11 19:23:09 +0545

    api error change

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/APIError.js";
 import { User } from "./../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
