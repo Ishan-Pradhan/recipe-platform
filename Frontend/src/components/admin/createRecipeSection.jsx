7c4c0793f5899db531a45dd1547dc197ce843f0e
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-02-13 10:12:39 +0545

    rollback

--- Frontend/src/components/admin/createRecipeSection.jsx
+++ Frontend/src/components/admin/createRecipeSection.jsx
@@ -4,6 +4,7 @@ import "../../index.css";
 import "react-quill/dist/quill.snow.css";
 import axios from "axios";
 import Quill from "quill";
+URL;
 import toast from "react-hot-toast";
 import { URL } from "../../constants/constants";
 
@@ -49,6 +50,7 @@ const modules = {
     ],
     ["link", "image", "video"],
     [{ align: [] }],
+
     ["clean"],
   ],
 };
@@ -69,10 +71,6 @@ const formats = [
   "background",
 ];
 
-// Max file size in bytes (4.5 MB)
-const MAX_FILE_SIZE_MB = 4.5;
-const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
-
 const CreateRecipeSection = () => {
   const [recipe, setRecipe] = useState({
     name: "",
@@ -90,8 +88,6 @@ const CreateRecipeSection = () => {
     category: "",
   });
   const [isLoading, setIsLoading] = useState(false);
-  const [imageSize, setImageSize] = useState(null);
-  const [isFileTooLarge, setIsFileTooLarge] = useState(false);
 
   const categories = [
     "Vegan",
@@ -136,24 +132,10 @@ const CreateRecipeSection = () => {
   };
 
   const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-
-    if (file) {
-      const fileSize = file.size;
-      setImageSize(fileSize);
-
-      // Check if file size exceeds the limit
-      if (fileSize > MAX_FILE_SIZE_BYTES) {
-        setIsFileTooLarge(true);
-      } else {
-        setIsFileTooLarge(false);
-      }
-
-      setRecipe((prevRecipe) => ({
-        ...prevRecipe,
-        image: file,
-      }));
-    }
+    setRecipe((prevRecipe) => ({
+      ...prevRecipe,
+      image: e.target.files[0], // This is the file selected by the user
+    }));
   };
 
   const handleAddNutrient = () => {
@@ -173,18 +155,9 @@ const CreateRecipeSection = () => {
     }));
   };
 
-  const formatFileSize = (sizeInBytes) => {
-    if (!sizeInBytes) return "";
-    if (sizeInBytes < 1024) return `${sizeInBytes} bytes`;
-    if (sizeInBytes < 1024 * 1024)
-      return `${(sizeInBytes / 1024).toFixed(2)} KB`;
-    return `${(sizeInBytes / (1024 * 1024)).toFixed(2)} MB`;
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-
     const filteredIngredients = recipe.ingredients.filter(
       (ingredient) => ingredient.trim() !== ""
     );
@@ -194,7 +167,6 @@ const CreateRecipeSection = () => {
     const filteredNutrients = recipe.nutrients.filter(
       (nutrient) => nutrient.name.trim() !== "" && nutrient.value.trim() !== ""
     );
-
     const formData = new FormData();
     formData.append("name", recipe.name);
     formData.append("smallIntro", recipe.smallIntro);
@@ -235,13 +207,19 @@ const CreateRecipeSection = () => {
   return (
     <section className="container mx-auto mb-10">
       <form
-        className="md:w-2/3 mx-auto md:p-14 p-4 flex flex-col gap-8 rounded-[24px] border-dark border-opacity-40"
+        className="md:w-2/3 mx-auto md:p-14 p-4  flex flex-col  gap-8  rounded-[24px] border-dark border-opacity-40"
         onSubmit={handleSubmit}
       >
         <div className="flex flex-col gap-10 justify-center md:items-center">
-          <h2 className="uppercase text-xl font-semibold text-center md:text-left">
-            Create Recipe
-          </h2>
+          <div className="flex flex-col gap-2 text-center">
+            <h2 className="uppercase text-xl font-semibold text-center">
+              Create Recipe
+            </h2>
+            <span className="text-sm text-primaryRed">
+              <i className="fa-solid fa-circle-info"></i> The whole file must be
+              under 4 mb.
+            </span>
+          </div>
 
           <div className="flex flex-col gap-2 w-full">
             <label className="uppercase font-semibold text-dark ">
@@ -257,189 +235,173 @@ const CreateRecipeSection = () => {
               className="rounded-[7px] p-4 "
             />
           </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
-              Small Introduction
+          <div className="flex flex-col md:flex-row md:items-center gap-2 w-full md:bg-[#fff] p-2 md:px-4 rounded-[7px]">
+            <label className="uppercase font-semibold text-dark ">
+              Upload Image
             </label>
             <input
-              type="text"
+              type="file"
+              accept="image/*"
+              name="image"
+              onChange={handleImageUpload}
+              required
+              className=""
+            />
+          </div>
+
+          <div className="flex flex-col gap-2 w-full">
+            <label className="uppercase font-semibold text-dark ">
+              Small Intro
+            </label>
+            <textarea
               name="smallIntro"
               value={recipe.smallIntro}
               onChange={handleChange}
-              placeholder="Enter a short introduction"
+              placeholder="Enter small intro"
               required
-              className="rounded-[7px] p-4 "
+              className="rounded-[7px] p-4"
             />
           </div>
+          <div className="flex flex-col md:flex-row gap-6  justify-between md:w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <label className="uppercase font-semibold text-dark ">
+                Prep Time
+              </label>
+              <input
+                type="text"
+                name="prepTime"
+                value={recipe.prepTime}
+                onChange={handleChange}
+                placeholder="Prep Time"
+                className="rounded-[7px] p-4"
+              />
+            </div>
 
-          <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
-              Preparation Time
-            </label>
-            <input
-              type="text"
-              name="prepTime"
-              value={recipe.prepTime}
-              onChange={handleChange}
-              placeholder="Enter preparation time"
-              required
-              className="rounded-[7px] p-4 "
-            />
+            <div className="flex flex-col gap-2 w-full">
+              <label className="uppercase font-semibold text-dark ">
+                Prep Level
+              </label>
+              <input
+                type="text"
+                name="prepLevel"
+                value={recipe.prepLevel}
+                onChange={handleChange}
+                placeholder="Prep Level"
+                className="rounded-[7px] p-4"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full">
+              <label className="uppercase font-semibold text-dark ">
+                Serves
+              </label>
+              <input
+                type="text"
+                name="serves"
+                value={recipe.serves}
+                onChange={handleChange}
+                placeholder="Serves"
+                className="rounded-[7px] p-4"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row justify-between w-full">
+            <div className="flex flex-col gap-2 justify-between ">
+              <label className="uppercase font-semibold text-dark ">
+                Ingredients
+              </label>
+              {recipe.ingredients.map((ingredient, index) => (
+                <div className="flex gap-6" key={index}>
+                  <input
+                    type="text"
+                    value={ingredient}
+                    data-index={index}
+                    onChange={(e) => handleArrayChange(e, "ingredients")}
+                    placeholder={`Ingredient ${index + 1}`}
+                    className="rounded-[7px] p-4"
+                  />
+                  {index === recipe.ingredients.length - 1 && (
+                    <button
+                      type="button"
+                      onClick={() => handleAddField("ingredients")}
+                      className="add-field-button text-primaryRed"
+                    >
+                      Add More
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2 ">
+              <label className="uppercase font-semibold text-dark ">
+                Equipment Needed
+              </label>
+              {recipe.equipments.map((equipment, index) => (
+                <div key={index} className="flex gap-6 input-group">
+                  <input
+                    type="text"
+                    value={equipment}
+                    data-index={index}
+                    onChange={(e) => handleArrayChange(e, "equipments")}
+                    placeholder={`Equipment ${index + 1}`}
+                    className="rounded-[7px] p-4"
+                  />
+                  {index === recipe.equipments.length - 1 && (
+                    <button
+                      type="button"
+                      onClick={() => handleAddField("equipments")}
+                      className="add-field-button text-primaryRed "
+                    >
+                      Add More
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
 
           <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
-              Preparation Level
-            </label>
-            <input
-              type="text"
-              name="prepLevel"
-              value={recipe.prepLevel}
-              onChange={handleChange}
-              placeholder="Enter preparation level"
-              required
-              className="rounded-[7px] p-4 "
-            />
-          </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">Serves</label>
-            <input
-              type="text"
-              name="serves"
-              value={recipe.serves}
-              onChange={handleChange}
-              placeholder="Enter the number of servings"
-              required
-              className="rounded-[7px] p-4 "
-            />
-          </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
-              Ingredients
-            </label>
-            {recipe.ingredients.map((ingredient, index) => (
-              <div key={index} className="flex items-center gap-4">
-                <input
-                  type="text"
-                  data-index={index}
-                  value={ingredient}
-                  onChange={(e) => handleArrayChange(e, "ingredients")}
-                  className="rounded-[7px] p-4 w-full"
-                  placeholder={`Ingredient #${index + 1}`}
-                />
-              </div>
-            ))}
-            <button
-              type="button"
-              onClick={() => handleAddField("ingredients")}
-              className="text-primaryRed mt-2"
-            >
-              Add Ingredient
-            </button>
-          </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
-              Equipments
-            </label>
-            {recipe.equipments.map((equipment, index) => (
-              <div key={index} className="flex items-center gap-4">
-                <input
-                  type="text"
-                  data-index={index}
-                  value={equipment}
-                  onChange={(e) => handleArrayChange(e, "equipments")}
-                  className="rounded-[7px] p-4 w-full"
-                  placeholder={`Equipment #${index + 1}`}
-                />
-              </div>
-            ))}
-            <button
-              type="button"
-              onClick={() => handleAddField("equipments")}
-              className="text-primaryRed mt-2"
-            >
-              Add Equipment
-            </button>
-          </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
-              Nutrients
+            <label className="uppercase font-semibold text-dark ">
+              Nutritional Information
             </label>
             {recipe.nutrients.map((nutrient, index) => (
-              <div key={index} className="flex gap-4">
+              <div
+                key={index}
+                className="flex flex-col md:flex-row gap-2 mb-4 md:mb-0 w-full"
+              >
                 <input
                   type="text"
                   name="name"
-                  data-index={index}
                   value={nutrient.name}
                   onChange={(e) => handleNutrientChange(e, index)}
-                  className="rounded-[7px] p-4 w-full"
-                  placeholder={`Nutrient name #${index + 1}`}
+                  placeholder="Nutrient Name"
+                  className="rounded-[7px] p-4"
                 />
                 <input
                   type="text"
                   name="value"
-                  data-index={index}
                   value={nutrient.value}
                   onChange={(e) => handleNutrientChange(e, index)}
-                  className="rounded-[7px] p-4 w-full"
-                  placeholder={`Value for #${index + 1}`}
+                  placeholder="Nutrient Value with unit"
+                  className="rounded-[7px] p-4"
                 />
+                {index === recipe.nutrients.length - 1 && (
+                  <button
+                    type="button"
+                    onClick={handleAddNutrient}
+                    className="add-field-button text-primaryRed"
+                  >
+                    Add Nutrient
+                  </button>
+                )}
               </div>
             ))}
-            <button
-              type="button"
-              onClick={handleAddNutrient}
-              className="text-primaryRed mt-2"
-            >
-              Add Nutrient
-            </button>
           </div>
 
           <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
-              Instructions
-            </label>
-            <ReactQuill
-              value={recipe.instructions}
-              onChange={handleEditorChange}
-              modules={modules}
-              formats={formats}
-              className="rounded-[7px] p-4"
-              placeholder="Enter your instructions"
-            />
-          </div>
-
-          <div className="flex gap-4 items-center">
-            <label className="flex items-center">
-              <input
-                type="checkbox"
-                name="featured"
-                checked={recipe.featured}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Featured
-            </label>
-            <label className="flex items-center">
-              <input
-                type="checkbox"
-                name="vegan"
-                checked={recipe.vegan}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Vegan
-            </label>
-          </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label className="uppercase font-semibold text-dark">
+            <label className="uppercase font-semibold text-dark ">
               Category
             </label>
             <select
@@ -449,20 +411,71 @@ const CreateRecipeSection = () => {
               required
               className="rounded-[7px] p-4"
             >
-              <option value="">Select Category</option>
-              {categories.map((category, index) => (
-                <option key={index} value={category}>
+              <option value="" disabled>
+                Select category
+              </option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
                   {category}
                 </option>
               ))}
             </select>
           </div>
 
-          <div className="flex flex-col gap-4 items-center mt-4">
+          <div className="flex flex-col gap-2 w-full">
+            <label className="uppercase font-semibold text-dark ">
+              Instructions
+            </label>
+            <ReactQuill
+              value={recipe.instructions}
+              onChange={handleEditorChange}
+              modules={modules}
+              formats={formats}
+              className="quill-editor"
+            />
+          </div>
+
+          <div className="flex w-full">
+            <div className="flex  gap-2 w-full">
+              <label
+                htmlFor="featured"
+                className="uppercase font-semibold text-dark "
+              >
+                Featured
+              </label>
+              <input
+                id="featured"
+                type="checkbox"
+                name="featured"
+                checked={recipe.featured}
+                onChange={handleChange}
+                className="rounded-[7px] p-4"
+              />
+            </div>
+
+            <div className="flex gap-2 w-full">
+              <label
+                htmlFor="vegan"
+                className="uppercase font-semibold text-dark "
+              >
+                Vegan
+              </label>
+              <input
+                id="vegan"
+                type="checkbox"
+                name="vegan"
+                checked={recipe.vegan}
+                onChange={handleChange}
+                className="rounded-[7px] p-4"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2 w-full">
             <button
               type="submit"
               className="bg-primaryRed text-light p-3 rounded-[7px]"
-              disabled={isFileTooLarge || isLoading}
+              disabled={isLoading}
             >
               {isLoading ? "Submitting..." : "Submit Recipe"}
             </button>
