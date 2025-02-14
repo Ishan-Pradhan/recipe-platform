f3d6d6ee7156fa3519fc7700d680c64401134a13
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-02-14 19:57:41 +0545

    responsive fix

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
       xl: "1280px",
 
       "2xl": "1440px",
+
+      "3xl": "1600px",
+
+      "4xl": "1800px",
+
+      "5xl": "2000px",
+
+      "6xl": "2200px",
+
+      "7xl": "2400px",
     },
 
     fontFamily: {
